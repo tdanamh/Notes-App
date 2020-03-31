b763bc7a2d37a066ee9062ca51f84aa2a83acabe
Dana Tudor <tdanamh@gmail.com>
2020-03-31 21:12:50 +0300

    Rewrite ProcessLogin Controller

--- src/main/java/com/example/demo/ProcessLoginController.java
+++ src/main/java/com/example/demo/ProcessLoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +11,24 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ProcessLoginController {
 
-    // Temporary harcoded credentials until creating Database and Models
-    private String emailDB = "dana@gmail.com", passDB = "1234";
+    @Autowired
+    private UserRepository repository;
+
+    private String emailDB, passDB;
 
     @PostMapping("/processLogin")
     public String processLogin(@RequestParam String email, @RequestParam String pass, Model model, HttpServletRequest request) {
-        if ( !email.equals(emailDB) || ! pass.equals(passDB)){
-            model.addAttribute("check", "Username or password incorrect!");
-            return "index";
+        User userFound = repository.findByEmail(email);
+        if (userFound != null) {
+            emailDB = userFound.email;
+            passDB = userFound.password;
+
+            if (email.equals(emailDB) && pass.equals(passDB)){
+                request.getSession().setAttribute("user", email);
+                return "redirect:/notes";
+            }
         }
-        request.getSession().setAttribute("user", email);
-        return "redirect:/notes";
+        model.addAttribute("check", "Username or password incorrect!");
+        return "index";
     }
 }
