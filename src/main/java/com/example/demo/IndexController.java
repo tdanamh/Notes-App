9a48607c805020e8477728906e2260a35583bc54
Dana Tudor <tdanamh@gmail.com>
2020-03-31 14:51:50 +0300

    Redirect to Notes Controller if session user field is set

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/index")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        if (request.getSession().getAttribute("user") != null){
+            return "redirect:/notes";
+        }
         return "index";
     }
 }
