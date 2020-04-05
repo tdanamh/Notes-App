f785348bea90679a36d2514401a4111cb18603b0
Dan <bdangabriel@gmail.com>
2020-04-05 22:18:52 +0300

    Add email configuration

---
+++ src/main/java/com/example/demo/MailNotifier.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+@Component
+public class MailNotifier {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendHtmlEmail(String title, String text, String receiver) throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        helper.setTo(receiver);
+
+        helper.setSubject("New note added");
+//        For simple text default = text/plain
+//        helper.setText("Check attachment for image!");
+
+//         true = text/html
+        helper.setText("Hello! You added a new note. <br>" +
+                "Title: " + title + "<br>" +
+                "Content: " + text + "<br>" +
+                " You can check it on the website.", true);
+
+        // For attachment
+        //FileSystemResource file = new FileSystemResource(new File("path/android.png"));
+
+        javaMailSender.send(msg);
+
+    }
+}
