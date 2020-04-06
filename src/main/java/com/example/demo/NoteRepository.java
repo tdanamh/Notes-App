5e88c201d97b0f0a57ce05a72d23f174e58fd2c9
rays00 <bdangabriel@gmail.com>
2020-04-06 21:05:40 +0300

    [WIP][DO NOT MERGE] Add view and controller for forget password functionality

--- src/main/java/com/example/demo/NoteRepository.java
+++ src/main/java/com/example/demo/NoteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface NoteRepository extends MongoRepository<Note, String> {
-    public Note findByTitle(String title);;
-    public List<Note> findByUserId(String userId);;
+    public Note findByTitle(String title);
+    public List<Note> findByUserId(String userId);
 }
