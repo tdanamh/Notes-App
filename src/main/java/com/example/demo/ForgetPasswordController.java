03b043459abbbae4095ff80d657cf8cf5c9642a5
rays00 <bdangabriel@gmail.com>
2020-04-07 20:02:46 +0300

    Completed forgot password functionality

--- src/main/java/com/example/demo/ForgetPasswordController.java
+++ src/main/java/com/example/demo/ForgetPasswordController.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ForgetPasswordController {
     @GetMapping("/forgetPassword")
-    public String register(){
+    public String forgetPassword(HttpServletRequest request, Model model) {
+        if (request.getSession().getAttribute("message") != null) {
+            model.addAttribute("check", request.getSession().getAttribute("message"));
+            request.getSession().setAttribute("message", null);
+        }
         return "forgetpassword";
     }
 }
---
+++ src/main/java/com/example/demo/ProcessForgetPasswordController.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class ProcessForgetPasswordController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    MailNotifier mailNotifier;
+
+    @PostMapping("/processForgetPassword")
+    public String processForgetPassword(@RequestParam String email, Model model, HttpServletRequest request)
+    {
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            try {
+                user.password = generateRandomPassword(8);
+                userRepository.save(user);
+                mailNotifier.sendHtmlEmail("Your password has been reset!",  user.password, "You can login using your new password.", email);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            request.getSession().setAttribute("message", "An email was sent at your email address with the new password.");
+        } else {
+            request.getSession().setAttribute("message", "We didn't find an account registered with your email. Maybe you would like to sign up.");
+        }
+        return "redirect:/forgetPassword";
+    }
+
+    private String generateRandomPassword(int size)
+    {
+        Random random = new Random();
+        StringBuilder newPassword = new StringBuilder(size);
+        for (int i = 0; i < size; i++) {
+            newPassword.append(random.nextInt(10));
+        }
+        return newPassword.toString();
+    }
+}
